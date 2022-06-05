9aabfb454b9273b3caf45a8f6f4040e894b0ace4
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2022-06-05 11:18:10 +0100

    added an orgin

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var cors = require("cors");
 var app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "https://jordansfreelanceing.netlify.app/contact" }));
 
 require("dotenv").config();
 
